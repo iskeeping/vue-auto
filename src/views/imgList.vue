ceeb82107bcbdd166177eecd766220ccde8a811a
tiemao <1778900272@qq.com>
2020-07-26 15:15:59 +0800

    删log

--- src/views/imgList.vue
+++ src/views/imgList.vue
@@ -145,14 +145,12 @@ export default {
       })
     },
     deleteOne(id) {
-      console.log(id)
       this.$confirm("是否确定删除？", "提示" , {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
       }).then( () => {
         api.imgDeleteOne({params:{_id: id}}).then((res) => {
-          console.log(res)
           if(res.data.code === 1) {
             this.$message.success("删除成功")
             this.imgGetList()
